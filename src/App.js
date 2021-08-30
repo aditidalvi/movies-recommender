8488f9bb6233cc6c4f8b3aaa0661b7762eca923b
Aditi Dalvi <67223320+aditidalvi@users.noreply.github.com>
2021-08-31 00:42:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import "./styles.css";
 import { useState } from "react";
 const moviesDictionary = {
   comedy: [
-    { name: "Hera Pheri", rating: "4.5/5" },
-    { name: "Hungama", rating: "4/5" },
+    {
+      name: "Hera Pheri",
+      description:
+        "Two tenants and a landlord, in desperate need of money, chance upon a ransom call via a cross connection. They hatch a plan to claim the ransom for themselves",
+      rating: "4.5/5"
+    },
+    {
+      name: "Hungama",
+      description:
+        "A millionaire businessman living in a village decides to move to the city with his wife. They soon get into a case of mistaken identity that results in comical situations.",
+      rating: "4/5"
+    },
     {
       name: "Home Alone",
+      description:
+        "When bratty 8-year-old Kevin McCallister (Macaulay Culkin) acts out the night before a family trip to Paris, his mother (Catherine O'Hara) makes him sleep in the attic. After the McCallisters mistakenly leave for the airport without Kevin, he awakens to an empty house and assumes his wish to have no family has come true. But his excitement sours when he realizes that two con men (Joe Pesci, Daniel Stern) plan to rob the McCallister residence, and that he alone must protect the family home.",
       rating: "4/5"
     }
   ],
@@ -13,28 +25,40 @@ const moviesDictionary = {
   action: [
     {
       name: "URI",
+      description:
+        "Major Vihaan Singh Shergill of the Indian Army leads a covert operation against a group of militants who attacked a base in Uri, Kashmir, in 2016 and killed many soldiers.",
       rating: "4.8/5"
     },
     {
       name: "Tanhaji",
+      description:
+        "Chhatrapati Shivaji's right-hand man, braveheart Subhedar Tanhaji Malusare, pits his sharp acumen against the brawn of ruthless and hedonistic Mughal chieftain Udaybhan Singh Rathore to recapture Kondhana for the Maratha empire",
       rating: "4.5/5"
     },
     {
       name: "Avengers Endgame",
+      description:
+        "Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.",
       rating: "4.8/5"
     }
   ],
   thriller: [
     {
       name: "Kahani",
+      description:
+        "A poison-gas attack on a Kolkata Metro Rail compartment kills the passengers on board. Two years later Vidya Bagchi, a pregnant British-Indian software engineer, arrives in Kolkata from London during the Durga Puja festivities in search of her missing husband, Arnab Bagchi.",
       rating: "4/5"
     },
     {
       name: "Drishyam",
+      description:
+        "When the disappearance of a policewoman's son threatens to ruin Vijay's family, he leaves no stone unturned in order to shield his family.",
       rating: "4/5"
     },
     {
       name: "Baby",
+      description:
+        "An elite team of the Indian intelligence system perpetually strives to detect and eliminate terrorists and their plots. Officer Ajay leads a team to destroy one such potentially lethal operation.",
       rating: "4/5"
     }
   ]
@@ -90,7 +114,7 @@ export default function App() {
                 <li
                   style={{
                     width: "60%",
-                    textAlign: "center",
+                    textAlign: "left",
                     border: "1px solid black",
                     borderRadius: "7px",
                     padding: "1.5rem",
@@ -108,7 +132,7 @@ export default function App() {
                     }}
                   >
                     {" "}
-                    {movies.name}{" "}
+                    Name:- {movies.name}{" "}
                   </div>
                   <div
                     style={{
@@ -118,7 +142,17 @@ export default function App() {
                     }}
                   >
                     {" "}
-                    {movies.rating}{" "}
+                    Film Synopsis:- {movies.description}{" "}
+                  </div>
+                  <div
+                    style={{
+                      fontSize: "medium",
+                      fontFamily: "Kaisei HarunoUmi",
+                      padding: "0.5rem 0"
+                    }}
+                  >
+                    {" "}
+                    Ratings:- {movies.rating}{" "}
                   </div>
                 </li>
               </ul>
